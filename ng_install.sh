c8823dfb3ef987eb265801c2b3cec694b4eafca4
NRGLine4Sec <NRGLine4Sec@yopmail.com>
2021-09-29 10:37:05 +0200

    update

--- ng_install.sh
+++ ng_install.sh
@@ -2120,7 +2120,7 @@ done
 
 exit 0
 EOF
-displayandexec "Installation du script de mise à jour de repo git   " "\
+displayandexec "Installation du script gitupdate                    " "\
 chmod +x /opt/gitupdate && \
 ln -s /opt/gitupdate /usr/bin/gitupdate"
 }
@@ -3457,10 +3457,67 @@ execandlog "mv /usr/lib/x86_64-linux-gnu/nautilus/extensions-3.0/libnautilus-wip
 ################################################################################
 ## configuration de l'audio
 ##------------------------------------------------------------------------------
-displayandexec "Désactivation du microphone                         " "$ExeAsUser amixer set Capture nocap"
-displayandexec "Réglage du volume audio à 10%                       " "$ExeAsUser amixer set Master 10%"
+displayandexec "Désactivation du microphone                         " "$ExeAsUser rm -rf /home/"$local_user"/.config/pulse/* && amixer set Capture nocap"
+displayandexec "Réglage du volume audio à 10%                       " "$ExeAsUser rm -rf /home/"$local_user"/.config/pulse/* && amixer set Master 10%"
 # Les deux commandes amixer ne fonctionnenet pas dans une install sur bullseye. A priori le problème serrait lié au fait qu'elles sont lancés depuis des sudo -u user.
 # Les commandes fonctionnement parfaitement si elles sont lancés depuis le user dans un terminal.
+# l'erreur peur avoir un lien avec la détection de carte audio, lorsqu'on ajoute une carte audio dummy dans la VM, on obtient une autre etteur lors de l'execution de amixer avec sudo :
+# ALSA lib simple_none.c:1544:(simple_add1) helem (MIXER,'Master Playback Switch',0,1,0) appears twice or more
+# amixer: Mixer default load error: Invalid argument
+
+# avec su ça fonctionne, mais su nous demande un mdp ce qui rend son execution interactif
+# si on effectue la commande dans un bash -c (bash -c "amixer set Master 10%"), ça fonctionne
+# par contre il ne fonctionne pas si on éffectue le bash -c dans un sudo
+
+# commandes utiles pour débugger :
+# $ExeAsUser apaly -l
+# $ExeAsUser apaly -L
+# $ExeAsUser amixer -c 0 controls
+# $ExeAsUser alsamixer -c 0
+
+# pacmd list-sinks
+# pacmd list-sinks | sed -n "/^.*name:/s/.*<\(.*\)>.*/\1/p"
+# la commande ci-dessous (qui fonctionne très bien sans le sudo) ne fonctionne pas lorsqu'elle est faite dans un sudo et nous renvois l'ereur suivante :
+# No PulseAudio daemon running, or not running as session daemon.
+# $ExeAsUser pacmd list-sinks
+# alors que cette même commande fonctionne lorsqu'elle est appelé par su (su "$local_user" -c "pacmd list-sinks")
+# pactl info -vvv
+# la commande ci-dessous (qui fonctionne très bien sans le sudo) ne fonctionne pas lorsqu'elle est faite dans un sudo et nous renvois l'ereur suivante :
+# cette commande fonctionne sur une buster
+# pa_context_connect() failed: Connection refused
+# Connection failure: Connection refused
+# $ExeAsUser pactl info -vvv
+
+# sans sudo :
+# pulseaudio --check -v
+# I: [pulseaudio] main.c: Daemon running as PID 14118
+
+# avec sudo :
+# $ExeAsUser pulseaudio --check -v
+# I: [pulseaudio] main.c: Daemon not running
+
+# a piori il y a le même comportement concernant le résultat de cette commmande sur une buster
+
+# Si on fait
+# $ExeAsUser pulseaudio --start -D
+# $ExeAsUser pulseaudio --check -v
+# I: [pulseaudio] main.c: Daemon running as PID 16983
+# $ExeAsUser amixer set Master 10%
+# $ExeAsUser pacmd list-sinks | grep '^[[:blank:]]volume:'
+# volume: front-left: 39322 /  60% / -13.31 dB,   front-right: 39322 /  60% / -13.31 dB
+# pacmd list-sinks | grep '^[[:blank:]]volume:'
+# volume: front-left: 6554 /  10% / -60.00 dB,   front-right: 6554 /  10% / -60.00 dB
+
+# et la, la commande fonctionne correctement, mais la modification ne s'applique pas au pulseaudio démaré par le système, mais s'applique au uniquement au pulseaudio démaré dans le sudo
+
+# $ExeAsUser pulseaudio -k && pulseaudio -vvvv
+
+# sudo fuser -v /dev/snd/*
+
+# systemctl --user status pulseaudio
+
+# cette dommande la a fonctionné dans le sudo : $ExeAsUser rm -rf /home/"$local_user"/.config/pulse/* &&  amixer set Master 20%
+
 ################################################################################
 
 
@@ -3605,199 +3662,6 @@ HISTTIMEFORMAT=\"%Y/%m/%d %T   \"
 EOF
 displayandexec "Configuration du bashrc                             " "stat /root/.bashrc && stat /home/$local_user/.bashrc"
 
-# $ExeAsUser cat>> /home/"$local_user"/.zshrc << 'EOF'
-$ExeAsUser cat>> /tmp/.zshrc << 'EOF'
-setopt autocd              # change directory just by typing its name
-#setopt correct            # auto correct mistakes
-setopt interactivecomments # allow comments in interactive mode
-setopt magicequalsubst     # enable filename expansion for arguments of the form ‘anything=expression’
-setopt nonomatch           # hide error message if there is no match for the pattern
-setopt notify              # report the status of background jobs immediately
-setopt numericglobsort     # sort filenames numerically when it makes sense
-setopt promptsubst         # enable command substitution in prompt
-
-WORDCHARS=${WORDCHARS//\/} # Don't consider certain characters part of the word
-
-# hide EOL sign ('%')
-PROMPT_EOL_MARK=""
-
-# configure key keybindings
-bindkey -e                                        # emacs key bindings
-bindkey ' ' magic-space                           # do history expansion on space
-bindkey '^[[3;5~' kill-word                       # ctrl + Supr
-bindkey '^[[3~' delete-char                       # delete
-bindkey '^[[1;5C' forward-word                    # ctrl + ->
-bindkey '^[[1;5D' backward-word                   # ctrl + <-
-bindkey '^[[5~' beginning-of-buffer-or-history    # page up
-bindkey '^[[6~' end-of-buffer-or-history          # page down
-bindkey '^[[H' beginning-of-line                  # home
-bindkey '^[[F' end-of-line                        # end
-bindkey '^[[Z' undo                               # shift + tab undo last action
-
-# enable completion features
-autoload -Uz compinit
-compinit -d ~/.cache/zcompdump
-zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' # case insensitive tab completion
-
-# History configurations
-HISTFILE=~/.zsh_history
-HISTSIZE=1000
-SAVEHIST=2000
-setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
-setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
-#setopt share_history         # share command history data
-
-# force zsh to show the complete history
-alias history="history 0"
-
-# make less more friendly for non-text input files, see lesspipe(1)
-#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# set variable identifying the chroot you work in (used in the prompt below)
-if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
-    debian_chroot=$(cat /etc/debian_chroot)
-fi
-
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color|*-256color) color_prompt=yes;;
-esac
-
-# uncomment for a colored prompt, if the terminal has the capability; turned
-# off by default to not distract the user: the focus in a terminal window
-# should be on the output of commands, not on the prompt
-force_color_prompt=yes
-
-if [ -n "$force_color_prompt" ]; then
-    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-	# We have color support; assume it's compliant with Ecma-48
-	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-	# a case would tend to support setf rather than setaf.)
-	color_prompt=yes
-    else
-	color_prompt=
-    fi
-fi
-
-if [ "$color_prompt" = yes ]; then
-    PROMPT=$'%F{%(#.blue.green)}┌──${debian_chroot:+($debian_chroot)──}(%B%F{%(#.red.blue)}%n%(#.💀.㉿)%m%b%F{%(#.blue.green)})-[%B%F{reset}%(6~.%-1~/…/%4~.%5~)%b%F{%(#.blue.green)}]\n└─%B%(#.%F{red}#.%F{blue}$)%b%F{reset} '
-    RPROMPT=$'%(?.. %? %F{red}%B⨯%b%F{reset})%(1j. %j %F{yellow}%B⚙%b%F{reset}.)'
-
-    # enable syntax-highlighting
-    if [ -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ] && [ "$color_prompt" = yes ]; then
-	. /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-	ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
-	ZSH_HIGHLIGHT_STYLES[default]=none
-	ZSH_HIGHLIGHT_STYLES[unknown-token]=fg=red,bold
-	ZSH_HIGHLIGHT_STYLES[reserved-word]=fg=cyan,bold
-	ZSH_HIGHLIGHT_STYLES[suffix-alias]=fg=green,underline
-	ZSH_HIGHLIGHT_STYLES[global-alias]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[precommand]=fg=green,underline
-	ZSH_HIGHLIGHT_STYLES[commandseparator]=fg=blue,bold
-	ZSH_HIGHLIGHT_STYLES[autodirectory]=fg=green,underline
-	ZSH_HIGHLIGHT_STYLES[path]=underline
-	ZSH_HIGHLIGHT_STYLES[path_pathseparator]=
-	ZSH_HIGHLIGHT_STYLES[path_prefix_pathseparator]=
-	ZSH_HIGHLIGHT_STYLES[globbing]=fg=blue,bold
-	ZSH_HIGHLIGHT_STYLES[history-expansion]=fg=blue,bold
-	ZSH_HIGHLIGHT_STYLES[command-substitution]=none
-	ZSH_HIGHLIGHT_STYLES[command-substitution-delimiter]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[process-substitution]=none
-	ZSH_HIGHLIGHT_STYLES[process-substitution-delimiter]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[back-quoted-argument]=none
-	ZSH_HIGHLIGHT_STYLES[back-quoted-argument-delimiter]=fg=blue,bold
-	ZSH_HIGHLIGHT_STYLES[single-quoted-argument]=fg=yellow
-	ZSH_HIGHLIGHT_STYLES[double-quoted-argument]=fg=yellow
-	ZSH_HIGHLIGHT_STYLES[dollar-quoted-argument]=fg=yellow
-	ZSH_HIGHLIGHT_STYLES[rc-quote]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[dollar-double-quoted-argument]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[back-double-quoted-argument]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[back-dollar-quoted-argument]=fg=magenta
-	ZSH_HIGHLIGHT_STYLES[assign]=none
-	ZSH_HIGHLIGHT_STYLES[redirection]=fg=blue,bold
-	ZSH_HIGHLIGHT_STYLES[comment]=fg=black,bold
-	ZSH_HIGHLIGHT_STYLES[named-fd]=none
-	ZSH_HIGHLIGHT_STYLES[numeric-fd]=none
-	ZSH_HIGHLIGHT_STYLES[arg0]=fg=green
-	ZSH_HIGHLIGHT_STYLES[bracket-error]=fg=red,bold
-	ZSH_HIGHLIGHT_STYLES[bracket-level-1]=fg=blue,bold
-	ZSH_HIGHLIGHT_STYLES[bracket-level-2]=fg=green,bold
-	ZSH_HIGHLIGHT_STYLES[bracket-level-3]=fg=magenta,bold
-	ZSH_HIGHLIGHT_STYLES[bracket-level-4]=fg=yellow,bold
-	ZSH_HIGHLIGHT_STYLES[bracket-level-5]=fg=cyan,bold
-	ZSH_HIGHLIGHT_STYLES[cursor-matchingbracket]=standout
-    fi
-else
-    PROMPT='${debian_chroot:+($debian_chroot)}%n@%m:%~%# '
-fi
-unset color_prompt force_color_prompt
-
-# If this is an xterm set the title to user@host:dir
-case "$TERM" in
-xterm*|rxvt*)
-    TERM_TITLE=$'\e]0;${debian_chroot:+($debian_chroot)}%n@%m: %~\a'
-    ;;
-*)
-    ;;
-esac
-
-new_line_before_prompt=yes
-precmd() {
-    # Print the previously configured title
-    print -Pnr -- "$TERM_TITLE"
-
-    # Print a new line before the prompt, but only if it is not the first line
-    if [ "$new_line_before_prompt" = yes ]; then
-	if [ -z "$_NEW_LINE_BEFORE_PROMPT" ]; then
-	    _NEW_LINE_BEFORE_PROMPT=1
-	else
-	    print ""
-	fi
-    fi
-}
-
-# enable color support of ls, less and man, and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-    alias diff='diff --color=auto'
-    alias ip='ip --color=auto'
-
-    export LESS_TERMCAP_mb=$'\E[1;31m'     # begin blink
-    export LESS_TERMCAP_md=$'\E[1;36m'     # begin bold
-    export LESS_TERMCAP_me=$'\E[0m'        # reset bold/blink
-    export LESS_TERMCAP_so=$'\E[01;33m'    # begin reverse video
-    export LESS_TERMCAP_se=$'\E[0m'        # reset reverse video
-    export LESS_TERMCAP_us=$'\E[1;32m'     # begin underline
-    export LESS_TERMCAP_ue=$'\E[0m'        # reset underline
-
-    # Take advantage of $LS_COLORS for completion as well
-    zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
-fi
-
-# some more ls aliases
-alias ll='ls -l'
-alias la='ls -A'
-alias l='ls -CF'
-
-# enable auto-suggestions based on the history
-if [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-    . /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-    # change suggestion color
-    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=#999'
-fi
-EOF
 displayandexec "Configuration du zshrc                              " "\
 rm -f /home/"$local_user"/.zshrc && \
 mv "$script_path"/.zshrc /home/"$local_user"/.zshrc && \
